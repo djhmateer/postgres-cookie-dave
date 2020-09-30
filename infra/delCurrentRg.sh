f2306a2dc14fd70e4134768d35496a416628429a
Dave Mateer <davemateer@gmail.com>
2020-09-30 17:15:52 +0100

    auto commit

--- infra/delCurrentRg.sh
+++ infra/delCurrentRg.sh
@@ -1,2 +1,2 @@
-az group delete --name PostgresCookieDaveTEST491 --no-wait --yes 
+az group delete --name PostgresCookieDaveTEST49 --no-wait --yes 
 
--- infra/sshCurrentVm.sh
+++ infra/sshCurrentVm.sh
@@ -1,3 +1,3 @@
 
-ssh -o StrictHostKeyChecking=no dave@postgrescookiedavetest491.westeurope.cloudapp.azure.com
+ssh -o StrictHostKeyChecking=no dave@postgrescookiedavetest49.westeurope.cloudapp.azure.com
 
